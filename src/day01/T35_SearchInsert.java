36587592c3eff0c3a8f8f6b54b6222d5e6d2db04
halley826 <janghalley826@gmail.com>
2021-12-14 13:25:56 +0800

    update day01 to day08

--- src/day01/T35_SearchInsert.java
+++ src/day01/T35_SearchInsert.java
@@ -3,7 +3,7 @@ package day01;
 /**
  * @author halley
  * @version 1.0
- * @description:
+ * @description:给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
  * @date 2021/12/7 5:08 下午
  */
 class T35_SearchInsert {

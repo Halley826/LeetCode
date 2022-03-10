d6e647fa19f10c66d335a241f4fff6a4c1c6a8cb
halley826 <janghalley826@gmail.com>
2022-03-10 17:01:49 +0800

    day09

--- src/day09/T9_isPalindrome.java
+++ src/day09/T9_isPalindrome.java
@@ -1,5 +1,7 @@
 package day09;
 
+import org.junit.Test;
+
 /**
  * @author halley
  * @version 1.0
@@ -7,4 +9,20 @@ package day09;
  * @date 2022/3/10 4:58 PM
  */
 public class T9_isPalindrome {
+    public boolean isPalindrome_2(int x) {
+        //方法二：将其倒过来，观察是否相等
+        if (x<0) return false;
+        if (x==0) return true;
+        int sum=x;
+        int cur=0;
+        while(sum>0){
+            cur=cur*10+sum%10;
+            sum=sum/10;
+        }
+        return cur==x;
+    }
+    @Test
+    public  void  test(){
+
+    }
 }
